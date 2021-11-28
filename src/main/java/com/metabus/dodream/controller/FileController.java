a6931c1e34b21d9374a97eaea5d7958a2a22a7c7
윤승규 <tuvisa96@gmail.com>
2021-11-28 20:43:06 +0900

    -파일 업로드 기능

--- src/main/java/com/metabus/dodream/controller/FileController.java
+++ src/main/java/com/metabus/dodream/controller/FileController.java
@@ -1,16 +1,48 @@
 package com.metabus.dodream.controller;
 
-import com.metabus.dodream.config.path.PathDeterminant;
+import com.google.gson.JsonObject;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.json.JSONException;
+import org.json.JSONObject;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.Map;
 
 @RestController
 @RequestMapping("/api/v1/dodream")
 @RequiredArgsConstructor
 @Slf4j
 public class FileController {
+    public static String FILE_PATH = "";
+    @PostMapping(value = "/upload", consumes = MediaType.MULTIPART_FORM_DATA_VALUE)
+    public ResponseEntity<String> upload(@RequestBody MultipartFile file) throws IOException, JSONException {
+        String fileName = file.getOriginalFilename();
 
-    PathDeterminant pathDeterminant = new PathDeterminant();
+        JsonObject param = new JsonObject();
+        if(!file.getOriginalFilename().isEmpty()) {
+            file.transferTo(new File(FILE_PATH, fileName));
+            param.addProperty("msg", "File uploaded successfully.");
+            param.addProperty("fileName", fileName);
+        }else {
+            param.addProperty("msg", "Please select a valid mediaFile..");
+        }
+
+        HttpHeaders response_header = new HttpHeaders();
+        response_header.set("content-type", "application/json");
+
+        JSONObject response = new JSONObject();
+        response.put("msg", param.get("msg"));
+        response.put("msg", param.get("fileName"));
+
+        return new ResponseEntity<>(response.toString(),response_header, HttpStatus.OK);
+
+    }
 }
